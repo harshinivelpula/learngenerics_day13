5a5aa50827728799bd85bf9a64f2ac2837a9db01
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-28 18:13:36 +0530

    printing array

---
+++ PrintArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    internal class PrintArray<T>
+    {
+        private T[] inputArray;
+        private PrintArray(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+
+        public void ToPrint()
+        {
+            foreach(var ele in inputArray)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.WriteLine("--------");
+        }
+
+        internal void toPrint()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
